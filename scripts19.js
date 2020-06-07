4f17dad1eba38b75d6247f23b1129f4ad2bcb7ae
picojunior <64814536+picojunior@users.noreply.github.com>
2020-06-07 15:08:58 -0500

    Update scripts19.js

--- scripts19.js
+++ scripts19.js
@@ -3,3 +3,15 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+  console.log(localMediaStream);
+  .then(localMediaStream => {
+    
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  });
+}
+
+getVideo();
